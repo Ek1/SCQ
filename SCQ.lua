ebd31042ecadb1c73ca141a052a109ea96634788
Erkki Seppä <CiO@Ek1.Fi>
2019-08-29 23:07:48 +0300

    0.9

--- SCQ.lua
+++ SCQ.lua
@@ -1,30 +1,107 @@
 local SCQ = {
-	TITLE = "Shares quests to party members that can contribute to the quest",	-- Not codereview friendly but enduser friendly version of the add-on's name
+	TITLE = "Share contributable quests",	-- Enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
-	DESCRIPTION = "Libary for other add-on's to get quest data.",
+	DESCRIPTION = "Shares quests to party members that can contribute to the quest.",
 	VERSION = "1.0",
 	LIECENSE = "BY-SA = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/SCQ"
 }
-local ADDON_NAME = "SCQ"	-- Variable used to refer to this add-on. Codereview friendly.
+local ADDON = "SCQ"	-- Codereview friendly referce to this add-on.
 
---[[ Loop through repeatable quests in the zone character is located and share those quests
-  ]]--
-function ShareZoneQuest (ZoneID) 
+-- Starting to do magic
+function SCQ.start()
 
-	IsJournalQuestInCurrentMapZone(number questIndex)
-	Returns: boolean isInCurrentZone
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED,	SCQ.inGroup)
 
+	d( SCQ.TITLE .. ": started. Listening EVENT_GROUP_MEMBER_JOINED")
 end
 --[[
-|H1:achievement:2491:12:0|h|h  |H1:achievement:2493:12:0|h|h
-/z =|H0:achievement:2492:51:0|h|h: Abode. Join the zoneGroup with /w + for |H0:item:151620:122:1:0:0:0:0:0:0:0:0:0:0:0:1:0:0:1:0:0:0|h|h.
-/z =|H0:achievement:2495:51:0|h|h: Duo. Join the zoneGroup with /w + for |H0:item:151620:122:1:0:0:0:0:0:0:0:1:0:0:0:1:0:0:1:0:0:0|h|h.
-/z +|H0:achievement:2498:-6571:0|h|h. Join zoneGroup with /w + for |H0:item:151623:122:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h.
-6357 ja 6384 /z =|H0:achievement:2496:1:0|h|h/6384. Join the zoneGroup with /w + for |H0:item:151623:122:50:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0|h|h.
+-- 100028 EVENT_GROUP_MEMBER_JOINED (number eventCode, string memberCharacterName, string memberDisplayName, boolean isLocalPlayer)
+function SCQ.inGroup(_ , _, _, isLocalPlayer)
 
-/z =|H0:achievement:2498:6357:0|h|h, =|H0:achievement:2495:51:0|h|h: Vhysradue & =|H0:achievement:2492:51:0|h|h: Tangle. Join the zoneGroup with /w @Ek1 + for shared progression, more quests and faster travel.
+	if isLocalPlayer then
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED)
+		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE,	SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
+		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT,	SCQ.soloing)
+		d( SCQ.TITLE .. ": inGroup, muting EVENT_GROUP_MEMBER_JOINED and listening EVENT_GROUP_SUPPORT_RANGE_UPDATE & EVENT_GROUP_MEMBER_LEFT.")
+	end
+end
+
+-- Following keeps track of the members that are in support range
+local groupMembersInSupportRange = {}
+local groupMembersInSupportRange[0] = 0
+-- 100028 EVENT_GROUP_SUPPORT_RANGE_UPDATE (number eventCode, string unitTag, boolean status)
+function SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE(_, unitTag, isSupporting)
+
+	if isSupporting then
+		groupMembersInSupportRange[0] = groupMembersInSupportRange[0] + 1
+	else
+		groupMembersInSupportRange[0] = groupMembersInSupportRange[0] - 1
+	end
+
+	d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE now " .. groupMembersInSupportRange[0] .. "party members in support range")
+
+	if 0 < groupMembersInSupportRange[0] then
+		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE,	SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
+		d( SCQ.TITLE .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+	else
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
+		d( SCQ.TITLE .. ": muting EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+	end
+end
+
+-- 100028 EVENT_QUEST_POSITION_REQUEST_COMPLETE (number eventCode, number taskId, MapDisplayPinType pinType, number xLoc, number yLoc, number areaRadius, boolean insideCurrentMapWorld, boolean isBreadcrumb)
+function SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE(_, taskId, pinType, xLoc, yLoc, areaRadius, insideCurrentMapWorld, isBreadcrumb)
+
+	local conditionData = self.taskIdToConditionData[taskId]
+	local journalQuestIndex, stepIndex, conditionIndex = conditionData.questIndex, conditionData.stepIndex, conditionData.conditionIndex
+
+	d( SCQ.TITLE .. ": EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+
+	if GetIsQuestSharable(journalQuestIndex) then
+		ShareQuest(journalQuestIndex)
+		d( SCQ.TITLE .. ": shared #" .. journalQuestIndex .. " " .. GetJournalQuestName(journalQuestIndex) )
+	end
+
+end
+
+function SCQ.soloing(_ , _, _, isLocalPlayer)
+
+	if isLocalPlayer then
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT)
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE)
+		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED,	SCQ.inGroup)
+		d( SCQ.TITLE .. ": soloing, muting EVENT_GROUP_MEMBER_LEFT, EVENT_GROUP_SUPPORT_RANGE_UPDATE and listening EVENT_GROUP_MEMBER_JOINED.")
+	end
+end
 ]]
+function SCQ.stop()
+
+	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED)
+	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE)
+	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
+	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT)
+
+	d( SCQ.TITLE .. ": stopped. Muting EVENT_GROUP_MEMBER_JOINED, EVENT_GROUP_SUPPORT_RANGE_UPDATE, EVENT_QUEST_POSITION_REQUEST_COMPLETE & EVENT_GROUP_MEMBER_LEFT")
+end
+
+-- Variable to keep count how many loads have been done before it was this ones turn.
+local loadOrder = 1
+function SCQ.onSCQLoaded(_, loadedAddOnName)
+	if loadedAddOnName == ADDON then
+	--	Seems it is our time so lets stop listening load trigger and initialize the add-on
+		d( SCQ.TITLE .. ": load order " ..  loadOrder .. ", starting")
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
+		SCQ.start()
+	end
+	loadOrder = loadOrder+1
+end
+
+-- Registering the SCQ's initializing event when add-on's are loaded 
+EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_ADD_ON_LOADED, SCQ.onSCQLoaded)
+
+--[[	Possible implementantion choices
+	
 
 GetGroupLeaderUnitTag()
 Returns: string leaderUnitTag
@@ -66,44 +143,17 @@ Returns: boolean isSoloOrGroupLeader
 JumpToGroupLeader()
 JumpToGroupMember(string characterOrDisplayName)
 
+IsJournalQuestInCurrentMapZone(number questIndex)
+Returns: boolean isInCurrentZone
+IsJournalQuestIndexInTrackedZoneStory(number journalQuestIndex)
+Returns: boolean isInTrackedZoneStory
+IsJournalQuestStepEnding(number journalQuestIndex, number stepIndex)
+Returns: boolean isEnding
+
+
+
 EVENT_GROUP_MEMBER_JOINED (number eventCode, string memberCharacterName, string memberDisplayName, boolean isLocalPlayer)	-- reset the list 
 EVENT_GROUP_UPDATE (number eventCode)	-- Fired when group size changes
 EVENT_GROUP_SUPPORT_RANGE_UPDATE (number eventCode, string unitTag, boolean status)	-- Fired when party member comes close enough(?) answer when to share quests?
 EVENT_LEADER_UPDATE (number eventCode, string leaderTag)
-
--- Lets fire up the add-on by registering for events and loading variables
-function SCQ.Initialize()
-
-	-- Loading account variables i.o. all quest with complete data or if none saved, create one
-	allQuestIds	= SCQ_allQuestIds or {}
-	allQuestNames	= SCQ_allQuestNames or {}
-
-	-- Loading character variables i.o. all incomplete quests
-	charactersQuestHistory	= ZO_SavedVars:NewCharacterIdSettings("SCQ_charactersQuestHistory", SCQ.VARIABLEVERSION, GetWorldName(), charactersQuestHistory) or {}
-	charactersOngoingQuests	= ZO_SavedVars:NewCharacterIdSettings("SCQ_ongoingCharacterQuests", SCQ.VARIABLEVERSION, GetWorldName(), charactersOngoingQuests) or {}
-
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_SHARED,	SCQ.EVENT_QUEST_SHARED)
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_OFFERED,	SCQ.EVENT_QUEST_OFFERED)
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_ADDED,	SCQ.EVENT_QUEST_ADDED)
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_ADVANCED,	SCQ.EVENT_QUEST_ADVANCED)
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_COMPLETE,	SCQ.EVENT_QUEST_COMPLETE)
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_REMOVED,	SCQ.EVENT_QUEST_REMOVED)
-
-
-	d( SCQ.TITLE .. ": initalization done. ")
-end
-
--- Variable to keep count how many loads have been done before it was this ones turn.
-local loadOrder = 1
-function SCQ.OnSCQLoaded(_, SCQName)
-	if SCQName == ADDON then
-	--	Seems it is our time so lets stop listening load trigger and initialize the add-on
-		d( SCQ.TITLE .. ": load order " ..  loadOrder .. ", starting initalization")
-		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
-		SCQ.Initialize()
-	end
-	loadOrder = loadOrder+1
-end
-
--- Registering the SCQ's initializing event when add-on's are loaded 
-EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_ADD_ON_LOADED, SCQ.OnSCQLoaded)
+]]

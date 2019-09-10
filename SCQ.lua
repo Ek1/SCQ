f5e9e2337d716609a80fcd06f88d04649b1c3d88
Erkki Seppä <CiO@Ek1.Fi>
2019-09-10 22:13:51 +0300

    1.0.190909.2013

--- SCQ.lua
+++ SCQ.lua
@@ -2,7 +2,7 @@ SCQ = {
 	TITLE = "Share contributable quests",	-- Enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
 	DESCRIPTION = "Shares quests to party members that can contribute to the quest.",
-	VERSION = "1.0.190909.1345",
+	VERSION = "1.0.190909.2013",
 	LIECENSE = "BY-SA = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/SCQ"
 }
@@ -51,20 +51,23 @@ end
 -- 100028 EVENT_GROUP_SUPPORT_RANGE_UPDATE (number eventCode, string unitTag, boolean status)
 function SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE(_, unitTag, isSupporting)
 
+	local GroupSize = GetGroupSize()
+
 	if isSupporting then
 		groupMembersInSupportRange = groupMembersInSupportRange + 1
-
-		local GroupSize = GetGroupSize()
-		if GroupSize <= groupMembersInSupportRange then
+		if GroupSize <= groupMembersInSupportRange then	-- there can't be more people supporting than there are members in group
 			groupMembersInSupportRange = GroupSize
 		end
 	else
 		groupMembersInSupportRange = groupMembersInSupportRange - 1
+		if groupMembersInSupportRange < 1 then	-- Player is always in supprot range of himself
+			groupMembersInSupportRange = 1
+		end
 	end
 
-	d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE now " .. groupMembersInSupportRange .. "party members in support range")
+--	d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE now " .. groupMembersInSupportRange .. "party members in support range")
 
-	if 1 < groupMembersInSupportRange then
+	if 2 <= groupMembersInSupportRange then
 --		ZO_PreHook(WORLD_MAP_QUEST_BREADCRUMBS, "OnQuestPositionRequestComplete", SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE, SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 --		d( ADDON .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
@@ -142,7 +145,7 @@ local loadOrder = 1
 function SCQ.GotLoaded(_, loadedAddOnName)
 	if loadedAddOnName == ADDON then
 	--	Seems it is our time so lets stop listening load trigger and initialize the add-on
-		d( SCQ.TITLE .. "(" .. ADDON .. ")".. ": load order " ..  loadOrder .. ", starting")
+		d( SCQ.TITLE .. " (" .. ADDON .. ")".. ": load order " ..  loadOrder .. ", starting")
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
 		SCQ.Start()
 	end

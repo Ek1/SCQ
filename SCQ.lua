97c930edb7de26cda8508fc594101d411e90713e
Erkki Seppä <CiO@Ek1.Fi>
2019-09-10 15:37:43 +0300

    1.0.190909.1337

--- SCQ.lua
+++ SCQ.lua
@@ -2,7 +2,7 @@ SCQ = {
 	TITLE = "Share contributable quests",	-- Enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
 	DESCRIPTION = "Shares quests to party members that can contribute to the quest.",
-	VERSION = "1.0.190909.1305",
+	VERSION = "1.0.190909.1337",
 	LIECENSE = "BY-SA = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/SCQ"
 }
@@ -40,10 +40,10 @@ function SCQ.EVENT_GROUP_MEMBER_JOINED(_ , _, memberDisplayName, isLocalPlayer)
 				groupMembersInSupportRange = groupMembersInSupportRange + 1
 			end
 		end
-		d( SCQ.TITLE .. ": joined a group.")
+		d( ADDON .. ": joined a group.")
 	else
 		SharedQuests = {}
-		d( SCQ.TITLE .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " joined the group.")
+		d( ADDON .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " joined the group.")
 	end
 end
 
@@ -68,10 +68,10 @@ function SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE(_, unitTag, isSupporting)
 	if 1 < groupMembersInSupportRange then
 --		ZO_PreHook(WORLD_MAP_QUEST_BREADCRUMBS, "OnQuestPositionRequestComplete", SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE, SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
-		d( SCQ.TITLE .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+--		d( ADDON .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
 	else
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
-		d( SCQ.TITLE .. ": muting EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+--		d( ADDON .. ": muting EVENT_QUEST_POSITION_REQUEST_COMPLETE")
 	end
 end
 
@@ -103,13 +103,13 @@ function SCQ.InnefficientSharing()
 		journalQuestName = GetJournalQuestName(i)
 
 		if SharedQuests[journalQuestName] then
---			d( SCQ.TITLE .. ": already shared before  #" .. i .. " " .. journalQuestName)
+--			d( ADDON .. ": already shared before  #" .. i .. " " .. journalQuestName)
 		else
 			if GetIsQuestSharable(i) then
 				if IsJournalQuestInCurrentMapZone(i) then
 					ShareQuest(i)
 					SharedQuests[journalQuestName] = os.time()
-					d( SCQ.TITLE .. ": shared #" .. i .. " " .. journalQuestName )
+					d( ADDON .. ": shared #" .. i .. " " .. journalQuestName )
 				end
 			else
 				SharedQuests[journalQuestName] = os.time()
@@ -122,9 +122,9 @@ end	-- Dirty quest sharing done
 function SCQ.EVENT_GROUP_MEMBER_LEFT(_ , memberCharacterName, GroupLeaveReason, isLocalPlayer, isLeader, memberDisplayName, actionRequiredVote)
 
 	if isLocalPlayer then
-		d( SCQ.TITLE .. ": soloing.")
+		d( ADDON .. ": soloing.")
 	else
-		d( SCQ.TITLE .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " left the group.")
+		d( ADDON .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " left the group.")
 	end
 end
 
@@ -135,7 +135,7 @@ function SCQ.Stop()
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT)
 
-	d( SCQ.TITLE .. ": stopped. Deff to EVENT_GROUP_MEMBER_JOINED, EVENT_GROUP_SUPPORT_RANGE_UPDATE, EVENT_QUEST_POSITION_REQUEST_COMPLETE & EVENT_GROUP_MEMBER_LEFT")
+	d( ADDON .. ": stopped. Deff to EVENT_GROUP_MEMBER_JOINED, EVENT_GROUP_SUPPORT_RANGE_UPDATE, EVENT_QUEST_POSITION_REQUEST_COMPLETE & EVENT_GROUP_MEMBER_LEFT")
 end
 
 -- Variable to keep count how many loads have been done before it was this ones turn.
@@ -143,7 +143,7 @@ local loadOrder = 1
 function SCQ.GotLoaded(_, loadedAddOnName)
 	if loadedAddOnName == ADDON then
 	--	Seems it is our time so lets stop listening load trigger and initialize the add-on
-		d( SCQ.TITLE .. ": load order " ..  loadOrder .. ", starting")
+		d( SCQ.TITLE .. "(" .. ADDON .. ")".. ": load order " ..  loadOrder .. ", starting")
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
 		SCQ.Start()
 	end

012d5f502f5aa1a4068fe99d2b4635810f5eb491
Erkki Seppä <CiO@Ek1.Fi>
2019-09-10 15:45:25 +0300

    1.0.190909.1345

--- SCQ.lua
+++ SCQ.lua
@@ -2,7 +2,7 @@ SCQ = {
 	TITLE = "Share contributable quests",	-- Enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
 	DESCRIPTION = "Shares quests to party members that can contribute to the quest.",
-	VERSION = "1.0.190909.1337",
+	VERSION = "1.0.190909.1345",
 	LIECENSE = "BY-SA = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/SCQ"
 }
@@ -52,12 +52,11 @@ end
 function SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE(_, unitTag, isSupporting)
 
 	if isSupporting then
-		local GroupSize = GetGroupSize()
+		groupMembersInSupportRange = groupMembersInSupportRange + 1
 
+		local GroupSize = GetGroupSize()
 		if GroupSize <= groupMembersInSupportRange then
 			groupMembersInSupportRange = GroupSize
-		else
-			groupMembersInSupportRange = groupMembersInSupportRange + 1
 		end
 	else
 		groupMembersInSupportRange = groupMembersInSupportRange - 1

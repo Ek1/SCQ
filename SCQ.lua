706ed303cf6f76e45d49b8c5c41783f04090006a
Erkki Seppä <CiO@Ek1.Fi>
2019-09-25 03:34:37 +0300

    1.1.190925.0134

--- SCQ.lua
+++ SCQ.lua
@@ -2,7 +2,7 @@ SCQ = {
 	TITLE = "Share contributable quests",	-- Enduser friendly version of the add-on's name
 	AUTHOR = "Ek1",
 	DESCRIPTION = "Shares quests to party members that can contribute to the quest.",
-	VERSION = "1.1.190922.0339",
+	VERSION = "1.1.190925.0134",
 	LIECENSE = "BY-SA = Creative Commons Attribution-ShareAlike 4.0 International License",
 	URL = "https://github.com/Ek1/SCQ"
 }
@@ -12,6 +12,7 @@ local ADDON = "SCQ"	-- Codereview friendly referce to this add-on.
 function SCQ.Start()
 
 	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED,	SCQ.EVENT_GROUP_MEMBER_JOINED)
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_PLAYER_ACTIVATED,	SCQ.EVENT_PLAYER_ACTIVATED)
 	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE,	SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE)
 	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT,	SCQ.EVENT_GROUP_MEMBER_LEFT)
 
@@ -19,7 +20,6 @@ function SCQ.Start()
 end
 
 local SharedQuests = {}
-
 -- API 100026	EVENT_QUEST_ADDED (number eventCode, number journalIndex, string questName, string objectiveName)
 function SCQ.EVENT_QUEST_ADDED (_, journalIndex, questName, objectiveName)
 	if GetIsQuestSharable(journalIndex) then
@@ -35,23 +35,37 @@ local groupMembersInSupportRange
 -- 100028 EVENT_GROUP_MEMBER_JOINED (number eventCode, string memberCharacterName, string memberDisplayName, boolean isLocalPlayer)
 function SCQ.EVENT_GROUP_MEMBER_JOINED(_ , _, memberDisplayName, isLocalPlayer)
 
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE,	SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE)
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_CONNECTED_STATUS,	SCQ.EVENT_GROUP_MEMBER_CONNECTED_STATUS)
+
 	if isLocalPlayer then
 		d( ADDON .. ": joined a group.")
 	else
 		SharedQuests = {}
 		d( ADDON .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " joined the group.")
 	end
+end
 
+--	EVENT_GROUP_MEMBER_CONNECTED_STATUS (number eventCode, string unitTag, boolean isOnline)
+function SCQ.EVENT_GROUP_MEMBER_CONNECTED_STATUS (_, string_unitTag, boolean_isOnline)
+	SCQ.fixMembersInSupportRange()
+end
+
+--	EVENT_PLAYER_ACTIVATED (number eventCode, boolean initial)
+function SCQ.EVENT_PLAYER_ACTIVATED(_, _)
+	SCQ.fixMembersInSupportRange()
+end
+
+--	Inefficient support range checker that nees to be done when exiting loading screen as EVENT_GROUP_SUPPORT_RANGE_UPDATE is not updated then
+function SCQ.fixMembersInSupportRange()
+	local GroupSize = GetGroupSize() or 0
 	groupMembersInSupportRange = 0
-	for i = 1, GetGroupSize() do
-
-		local selectedUnitTag = GetGroupUnitTagByIndex(i)
-
-		if IsUnitInGroupSupportRange( selectedUnitTag ) then
-			groupMembersInSupportRange = groupMembersInSupportRange + 1
-		end
-		if IsUnitPlayer(selectedUnitTag) then
-			playerUnitTag = selectedUnitTag
+	if 0 < GroupSize then
+		for i = 1, GroupSize do
+			local scopum = GetGroupUnitTagByIndex(i)
+			if IsUnitInGroupSupportRange( scopum ) then
+				groupMembersInSupportRange = groupMembersInSupportRange + 1
+			end
 		end
 	end
 end
@@ -60,32 +74,37 @@ end
 -- 100028 EVENT_GROUP_SUPPORT_RANGE_UPDATE (number eventCode, string unitTag, boolean status)
 function SCQ.EVENT_GROUP_SUPPORT_RANGE_UPDATE(_, unitTag, isSupporting)
 
-	local GroupSize = GetGroupSize() or 1
 	if not groupMembersInSupportRange then
-		groupMembersInSupportRange = 1
+		groupMembersInSupportRange = 0
 	end
+	
+	local GroupSize = GetGroupSize() or 2	-- 
+	d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE " .. groupMembersInSupportRange .. " party members in support range before check")
 
 	if isSupporting then
-		groupMembersInSupportRange = groupMembersInSupportRange + 1 or 2
+		groupMembersInSupportRange = groupMembersInSupportRange + 1
+		d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE updated to " .. groupMembersInSupportRange .. " party members in support range")
 		if GroupSize and GroupSize < groupMembersInSupportRange then	-- There can't be more people supporting than there are members in group
 			groupMembersInSupportRange = GroupSize or 2
+			d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE fixed to " .. groupMembersInSupportRange .. " party members in support range")
 		end
 	else
 		groupMembersInSupportRange = groupMembersInSupportRange - 1
+		d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE updated to " .. groupMembersInSupportRange .. " party members in support range")
 		if groupMembersInSupportRange < 1 then	-- Player is always in support range of himself
 			groupMembersInSupportRange = 1
+			d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE fixed to " .. groupMembersInSupportRange .. " party members in support range")
 		end
 	end
 
-	-- d( SCQ.TITLE .. ": EVENT_GROUP_SUPPORT_RANGE_UPDATE now " .. groupMembersInSupportRange .. "party members in support range")
 
 	if 2 <= groupMembersInSupportRange then
 	-- ZO_PreHook(WORLD_MAP_QUEST_BREADCRUMBS, "OnQuestPositionRequestComplete", SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE, SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE)
-	-- d( ADDON .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+		d( ADDON .. ": listening to EVENT_QUEST_POSITION_REQUEST_COMPLETE")
 	else
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
-	-- d( ADDON .. ": muting EVENT_QUEST_POSITION_REQUEST_COMPLETE")
+		d( ADDON .. ": muting EVENT_QUEST_POSITION_REQUEST_COMPLETE")
 	end
 end
 
@@ -93,7 +112,9 @@ local playersCurrentZoneIndex = GetUnitZoneIndex("player") or -1
 -- 100028 EVENT_QUEST_POSITION_REQUEST_COMPLETE (number eventCode, number taskId, MapDisplayPinType pinType, number xLoc, number yLoc, number areaRadius, boolean insideCurrentMapWorld, boolean isBreadcrumb)
 function SCQ.EVENT_QUEST_POSITION_REQUEST_COMPLETE(self, taskId, pinType, xLoc, yLoc, areaRadius, insideCurrentMapWorld, isBreadcrumb)
 
-	if insideCurrentMapWorld and DoesCurrentMapMatchMapForPlayerLocation() then
+	if insideCurrentMapWorld and DoesCurrentMapMatchMapForPlayerLocation() and GetUnitZoneIndex("player") ~= playersCurrentZoneIndex then
+		d(taskId)
+		playersCurrentZoneIndex = GetUnitZoneIndex("player")
 		SCQ.InnefficientSharing()
 	end
 
@@ -117,7 +138,7 @@ end
 function SCQ.InnefficientSharing()
 
 	local journalQuestName
-	local playersCurrentZoneIndex = GetUnitZoneIndex("player")	-- Where we are now
+--	local playersCurrentZoneIndex = GetUnitZoneIndex("player")	-- Where we are now
 	d( ADDON .. ": playersCurrentZoneIndex = " .. playersCurrentZoneIndex)
 
 	for i = 1, GetNumJournalQuests() do
@@ -149,6 +170,8 @@ function SCQ.EVENT_GROUP_MEMBER_LEFT(_ , memberCharacterName, GroupLeaveReason, 
 		d( ADDON .. ": soloing.")
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
 		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT)
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE)
+		EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED,	SCQ.EVENT_GROUP_MEMBER_JOINED)
 	else
 		d( ADDON .. ": " .. ZO_LinkHandler_CreateDisplayNameLink(memberDisplayName)  .. " left the group.")
 	end
@@ -159,6 +182,7 @@ function SCQ.Stop()
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_JOINED)
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_SUPPORT_RANGE_UPDATE)
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_QUEST_POSITION_REQUEST_COMPLETE)
+	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_CONNECTED_STATUS)
 	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_GROUP_MEMBER_LEFT)
 
 	d( ADDON .. ": stopped. Deff to EVENT_GROUP_MEMBER_JOINED, EVENT_GROUP_SUPPORT_RANGE_UPDATE, EVENT_QUEST_POSITION_REQUEST_COMPLETE & EVENT_GROUP_MEMBER_LEFT")
@@ -193,11 +217,6 @@ Returns: boolean isInSameWorld
 IsUnitGroupLeader(string unitTag)
 Returns: boolean isGroupLeader
 
-GetGroupSize()
-Returns: number groupSize
-GetGroupUnitTagByIndex(number sortIndex)
-Returns: string:nilable unitTag
-
 GetGroupUnitTagByIndex(number sortIndex)
 Returns: string:nilable unitTag
 
@@ -221,23 +240,11 @@ Returns: boolean isGrouped
 GetCurrentCharacterId()
 Returns: string id
 
-IsUnitSoloOrGroupLeader(string unitTag)
-Returns: boolean isSoloOrGroupLeader
-
-JumpToGroupLeader()
-JumpToGroupMember(string characterOrDisplayName)
-
-IsJournalQuestInCurrentMapZone(number questIndex)
-Returns: boolean isInCurrentZone
 IsJournalQuestIndexInTrackedZoneStory(number journalQuestIndex)
 Returns: boolean isInTrackedZoneStory
+
 IsJournalQuestStepEnding(number journalQuestIndex, number stepIndex)
 Returns: boolean isEnding
 
-
-
-EVENT_GROUP_MEMBER_JOINED (number eventCode, string memberCharacterName, string memberDisplayName, boolean isLocalPlayer)	-- reset the list 
-EVENT_GROUP_MEMBER_LEFT (number eventCode, string memberCharacterName, GroupLeaveReason reason, boolean isLocalPlayer, boolean isLeader, string memberDisplayName, boolean actionRequiredVote)
-EVENT_GROUP_SUPPORT_RANGE_UPDATE (number eventCode, string unitTag, boolean status)	-- Fired when party member comes close enough(?) answer when to share quests?
 EVENT_LEADER_UPDATE (number eventCode, string leaderTag)
 ]]
